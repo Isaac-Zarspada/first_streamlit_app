0dd8cfa5d444b7c645270bde3a2a01d4741aef90
Isaac Zarspada <Isaaczarspada@gmail.com>
2023-10-03 01:25:57 -0500

    spelling

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 streamlit.header("The fruit load list contains:")
-streamlist.stop()
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
